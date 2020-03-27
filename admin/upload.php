fd30685e3b0793c7f6ab676970b46dc4a6c846a7
Andreas Henningson <henningsonandreas@gmail.com>
2020-03-27 13:02:01 +0100

    Added radio button images and styles

--- admin/upload.php
+++ admin/upload.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "db.php";
+require_once "db.php";
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -22,7 +22,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -39,7 +39,13 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        $sql = "INSERT INTO blog_images (image)
+        VALUES (:image)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":image", $target_file);
+        $stmt->execute();
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+
